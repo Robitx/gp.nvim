da6da87f727b97ef07e16f406673b9d50b1d82c7
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-06-18 20:04:00 +0200

    feat: auto mkdir for _dir conf vars

--- lua/gp.lua
+++ lua/gp.lua
@@ -17,7 +17,7 @@ M.error = function(msg)
 end
 
 -- default config also serving as documentation example
-M.config = {
+local config = {
 	-- default prefix for all commands
 	cmd_prefix = "G",
 	-- example hook functions
@@ -26,6 +26,7 @@ M.config = {
 			print(string.format("%s plugin structure:\n%s", M._Name, vim.inspect(plugin)))
 		end,
 	},
+	chat_dir = os.getenv("HOME") .. "/.local/share/nvim/gp/chats",
 }
 
 -- setup function
@@ -36,6 +37,9 @@ M.setup = function(opts)
 		M.error(string.format("setup() expects table, but got %s:\n%s", type(opts), vim.inspect(opts)))
 		opts = {}
 	end
+
+	-- reset M.config
+	M.config = vim.deepcopy(config)
 
 	-- mv default M.config.hooks to M.cmd_hooks
 	for k, v in pairs(M.config.hooks) do
@@ -56,13 +60,22 @@ M.setup = function(opts)
 		M.config[k] = v
 	end
 
-	-- register commands
+	-- make sure _dirs exists
+	for k, v in pairs(M.config) do
+		if k:match("_dir$") and vim.fn.isdirectory(v) == 0 then
+			print(M._Name .. ": creating directory " .. v)
+			vim.fn.mkdir(v, "p")
+		end
+	end
+
+	-- register user commands
 	for hook, _ in pairs(M.cmd_hooks) do
 		vim.api.nvim_create_user_command(M.config.cmd_prefix .. hook, function()
 			M.call_hook(hook)
 		end, { nargs = "?", range = (hook:match("^Visual") ~= nil), desc = "GPT Prompt plugin" })
 	end
 
+    -- register default commands
 	for cmd, _ in pairs(M.cmd) do
 		if M.cmd_hooks[cmd] == nil then
 			vim.api.nvim_create_user_command(M.config.cmd_prefix .. cmd, function()
@@ -79,10 +92,10 @@ M.call_hook = function(name)
 	M.error("The hook '" .. name .. "' does not exist.")
 end
 
+--[[ M.setup({chat_dir = "/tmp/gp/chats"}) ]]
 --[[ M.setup() ]]
 --[[ M.setup("") ]]
 --[[ M.call_hook("InsectPlugin") ]]
 --[[ M.call_hook("InspectPlugin") ]]
-
 
 return M
