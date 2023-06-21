9c3925ea5c0b7a559401c3cfb1db8b5538ea35bc
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-06-21 18:17:55 +0200

    feat: stream gpt response into a buffer

--- lua/gp.lua
+++ lua/gp.lua
@@ -6,6 +6,8 @@ local _H = {}
 M = {
 	_Name = "Gp (GPT prompt)", -- plugin name
 	_H = _H, -- helper functions
+	_payload = {}, -- payload for openai api
+	_response = "", -- response from openai api
 	config = {}, -- config variables
 	cmd = {}, -- default command functions
 	cmd_hooks = {}, -- user defined command functions
@@ -18,6 +20,8 @@ end
 
 -- default config also serving as documentation example
 local config = {
+	-- required openai api key
+	openai_api_key = os.getenv("OPENAI_API_KEY"),
 	-- default prefix for all commands
 	cmd_prefix = "G",
 	-- example hook functions
@@ -75,13 +79,25 @@ M.setup = function(opts)
 		end, { nargs = "?", range = (hook:match("^Visual") ~= nil), desc = "GPT Prompt plugin" })
 	end
 
-    -- register default commands
+	-- register default commands
 	for cmd, _ in pairs(M.cmd) do
 		if M.cmd_hooks[cmd] == nil then
 			vim.api.nvim_create_user_command(M.config.cmd_prefix .. cmd, function()
 				M.cmd[cmd]()
 			end, { nargs = "?", range = (cmd:match("^Visual") ~= nil), desc = "GPT Prompt plugin" })
 		end
+	end
+
+	-- make sure curl is installed
+	if vim.fn.executable("curl") == 0 then
+		M.error("curl is not installed")
+		return
+	end
+
+	-- make sure openai_api_key is set
+	if M.config.openai_api_key == nil then
+		M.error("openai_api_key is not set")
+		return
 	end
 end
 
@@ -92,8 +108,127 @@ M.call_hook = function(name)
 	M.error("The hook '" .. name .. "' does not exist.")
 end
 
---[[ M.setup({chat_dir = "/tmp/gp/chats"}) ]]
+M.query = function(payload, handler, on_exit)
+	-- make sure handler is a function
+	if type(handler) ~= "function" then
+		M.error(string.format("query() expects handler function, but got %s:\n%s", type(handler), vim.inspect(handler)))
+		return
+	end
+
+	-- store payload for debugging
+	M._payload = payload
+
+	-- prepare pipes
+	local stdout = vim.loop.new_pipe(false)
+	local stderr = vim.loop.new_pipe(false)
+
+	-- spawn curl process
+	vim.loop.spawn("curl", {
+		args = {
+			"--no-buffer",
+			"-s",
+			"https://api.openai.com/v1/chat/completions",
+			"-H",
+			"Content-Type: application/json",
+			"-H",
+			"Authorization: Bearer " .. M.config.openai_api_key,
+			"-d",
+			vim.json.encode(payload),
+			--[[ "--doesnt_exist" ]]
+		},
+		stdio = { nil, stdout, stderr },
+	}, function(code, signal)
+		-- process cleanup
+		if code ~= 0 then
+			M.error(string.format("OpenAI query exited: %d, %d", code, signal))
+		end
+		vim.loop.read_stop(stdout)
+		vim.loop.read_stop(stderr)
+		vim.loop.close(stdout)
+		vim.loop.close(stderr)
+
+		-- optional on_exit handler
+		if type(on_exit) == "function" then
+			on_exit()
+		end
+	end)
+
+	-- read stdout
+	vim.loop.read_start(stdout, function(err, chunk)
+		if err then
+			M.error("OpenAI query stdout error: " .. vim.inspect(err))
+		end
+
+		if chunk then
+			-- iterate over lines
+			local lines = vim.split(chunk, "\n")
+			for _, line in ipairs(lines) do
+				-- parse out content for handler
+				line = line:gsub("^data: ", "")
+				if line:match("chat%.completion%.chunk") then
+					line = vim.json.decode(line)
+					local content = line.choices[1].delta.content
+					if content ~= nil then
+						-- store response for debugging
+						M._response = M._response .. content
+						-- call response handler
+						handler(content)
+					end
+				end
+			end
+		end
+	end)
+
+	-- read stderr
+	vim.loop.read_start(stderr, function(err, chunk)
+		if err then
+			M.error("OpenAI query stderr error: " .. vim.inspect(err))
+		end
+
+		if chunk then
+			print("stderr data: " .. vim.inspect(chunk))
+		end
+	end)
+end
+
+M.create_handler = function(buf, line, first_undojoin)
+	buf = buf or vim.api.nvim_get_current_buf()
+	local first_line = line or vim.api.nvim_win_get_cursor(0)[1] - 1
+	local skip_first_undojoin = not first_undojoin
+
+	local response = ""
+	return vim.schedule_wrap(function(chunk)
+		-- undojoin takes previous change into account, so skip it for the first chunk
+		if skip_first_undojoin then
+			skip_first_undojoin = false
+		else
+			vim.cmd("undojoin")
+		end
+
+		-- clean previous response
+		local line_count = #vim.split(response, "\n")
+		vim.api.nvim_buf_set_lines(buf, first_line, first_line + line_count, false, {})
+
+		-- append new response
+		response = response .. chunk
+		vim.cmd("undojoin")
+		vim.api.nvim_buf_set_lines(buf, first_line, first_line, false, vim.split(response, "\n"))
+	end)
+end
+
+M.cmd.Run = function()
+	local handler = M.create_handler()
+	M.query({
+		model = "gpt-3.5-turbo",
+		stream = true,
+		messages = { { role = "user", content = "Hi. Please tell me few short jokes." } },
+	}, handler)
+end
+
 --[[ M.setup() ]]
+--[[ print("gp.lua loaded\n\n") ]]
+
+--[[ M.setup({chat_dir = "/tmp/gp/chats"}) ]]
 --[[ M.setup("") ]]
 --[[ M.call_hook("InsectPlugin") ]]
 --[[ M.call_hook("InspectPlugin") ]]
