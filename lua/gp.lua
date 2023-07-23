7600c22f54507e76aa735a29f92bc79cabcfe2c9
Lintao <lintao.amons@outlook.com>
2023-07-23 19:19:38 +0800

    Allow GpChatResponse be triggered by files not in the chat folder

--- lua/gp.lua
+++ lua/gp.lua
@@ -784,13 +784,6 @@ end
 M.cmd.ChatRespond = function()
 	local buf = vim.api.nvim_get_current_buf()
 
-	-- check if file is in the chat dir
-	local file_name = vim.api.nvim_buf_get_name(buf)
-	if not string.match(file_name, M.config.chat_dir) then
-		print("File " .. file_name .. " is not in chat dir")
-		return
-	end
-
 	-- get all lines
 	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
 
