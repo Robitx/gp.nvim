633ca93a76d8a84eb3e0642dff5616206633831e
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2023-11-27 01:10:20 +0100

    docs: update hook examples

--- lua/gp/init.lua
+++ lua/gp/init.lua
@@ -206,21 +206,20 @@ local config = {
 		end,
 
 		-- your own functions can go here, see README for more examples like
-		-- :GpExplain, :GpUnitTests.., :GpBetterChatNew, ..
+		-- :GpExplain, :GpUnitTests.., :GpTranslator etc.
 
 		-- -- example of making :%GpChatNew a dedicated command which
 		-- -- opens new chat with the entire current buffer as a context
 		-- BufferChatNew = function(gp, _)
-		--     -- call GpChatNew command in range mode on whole buffer
-		--     vim.api.nvim_command("%" .. gp.config.cmd_prefix .. "ChatNew")
+		-- 	-- call GpChatNew command in range mode on whole buffer
+		-- 	vim.api.nvim_command("%" .. gp.config.cmd_prefix .. "ChatNew")
 		-- end,
 
-		-- -- example of adding a custom chat command with non-default parameters
-		-- -- (configured default might be gpt-3 and sometimes you might want to use gpt-4)
-		-- BetterChatNew = function(gp, params)
-		-- 	local chat_model = { model = "gpt-4", temperature = 0.7, top_p = 1 }
-		-- 	local chat_system_prompt = "You are a general AI assistant."
-		-- 	gp.cmd.ChatNew(params, chat_model, chat_system_prompt)
+		-- -- example of adding command which opens new chat dedicated for translation
+		-- Translator = function(gp, params)
+		-- 	local agent = gp.get_command_agent()
+		-- 	local chat_system_prompt = "You are a Translator, please translate between English and Chinese."
+		-- 	gp.cmd.ChatNew(params, agent.model, chat_system_prompt)
 		-- end,
 
 		-- -- example of adding command which writes unit tests for the selected code
@@ -228,8 +227,8 @@ local config = {
 		-- 	local template = "I have the following code from {{filename}}:\n\n"
 		-- 		.. "```{{filetype}}\n{{selection}}\n```\n\n"
 		-- 		.. "Please respond by writing table driven unit tests for the code above."
-		-- 	gp.Prompt(params, gp.Target.enew, nil, gp.config.command_model,
-		--         template, gp.config.command_system_prompt)
+		-- 	local agent = gp.get_command_agent()
+		-- 	gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
 		-- end,
 
 		-- -- example of adding command which explains the selected code
@@ -237,8 +236,8 @@ local config = {
 		-- 	local template = "I have the following code from {{filename}}:\n\n"
 		-- 		.. "```{{filetype}}\n{{selection}}\n```\n\n"
 		-- 		.. "Please respond by explaining the code above."
-		-- 	gp.Prompt(params, gp.Target.popup, nil, gp.config.command_model,
-		--         template, gp.config.chat_system_prompt)
+		-- 	local agent = gp.get_chat_agent()
+		-- 	gp.Prompt(params, gp.Target.popup, nil, agent.model, template, agent.system_prompt)
 		-- end,
 	},
 }
