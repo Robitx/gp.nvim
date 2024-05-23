6c0196a3a90537879f1605dc07de8b14af15d14f
argshook <argshook@gmail.com>
2024-05-23 11:21:31 +0300

    Update model parameters to GPT-4o in config
    - Set `model` parameter to "gpt-4o" in both chat and command configurations.

--- lua/gp/config.lua
+++ lua/gp/config.lua
@@ -40,7 +40,7 @@ local config = {
 			chat = true,
 			command = false,
 			-- string with model name or table with model name and parameters
-			model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
+			model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
 			-- system prompt (use this to specify the persona/role of the AI)
 			system_prompt = "You are a general AI assistant.\n\n"
 				.. "The user provided the additional info about how they would like you to respond:\n\n"
@@ -74,7 +74,7 @@ local config = {
 			chat = false,
 			command = true,
 			-- string with model name or table with model name and parameters
-			model = { model = "gpt-4-1106-preview", temperature = 0.8, top_p = 1 },
+			model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
 			-- system prompt (use this to specify the persona/role of the AI)
 			system_prompt = "You are an AI working as a code editor.\n\n"
 				.. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
