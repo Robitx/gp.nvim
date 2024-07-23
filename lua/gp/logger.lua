33812a62d6e3a34a10d24c696106337a5e2ef4b3
Tibor Schmidt <robitx@gmail.com>
2024-07-23 23:59:32 +0200

    feat: add logging to file (#166)
    * feat: add logging to file
    
    * chore: update README and auto-generate vimdoc
    
    * chore: fmt
    
    * chore: default log path to stdpath('log')
    
    * chore: support logger.trace
    
    * chore: update README and auto-generate vimdoc
    
    * chore: don't notify about debug and trace
    
    * chore: add uuid to log msgs to differentiate neovim instances
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ lua/gp/logger.lua
@@ -0,0 +1,78 @@
+local M = {}
+
+local file = "/dev/null"
+local uuid = ""
+
+M._log_history = {}
+
+---@param path string # path to log file
+M.set_log_file = function(path)
+	uuid = string.format("%x", math.random(0, 0xFFFF)) .. string.format("%x", os.time() % 0xFFFF)
+	M.debug("New neovim instance [" .. uuid .. "] started, setting log file to " .. path)
+	local dir = vim.fn.fnamemodify(path, ":h")
+	if vim.fn.isdirectory(dir) == 0 then
+		vim.fn.mkdir(dir, "p")
+	end
+	file = path
+
+	local log_file = io.open(file, "a")
+	if log_file then
+		for _, line in ipairs(M._log_history) do
+			log_file:write(line .. "\n")
+		end
+		log_file:close()
+	end
+end
+
+---@param msg string # message to log
+---@param level integer # log level
+---@param slevel string # log level as string
+local log = function(msg, level, slevel)
+	local raw = string.format("[%s] [%s] %s: %s", os.date("%Y-%m-%d %H:%M:%S"), uuid, slevel, msg)
+
+	M._log_history[#M._log_history + 1] = raw
+	if #M._log_history > 100 then
+		table.remove(M._log_history, 1)
+	end
+
+	local log_file = io.open(file, "a")
+	if log_file then
+		log_file:write(raw .. "\n")
+		log_file:close()
+	end
+
+	if level <= vim.log.levels.DEBUG then
+		return
+	end
+
+	vim.schedule(function()
+		vim.notify(msg, level, { title = "gp.nvim" })
+	end)
+end
+
+---@param msg string # error message
+M.error = function(msg)
+	log(msg, vim.log.levels.ERROR, "ERROR")
+end
+
+---@param msg string # warning message
+M.warning = function(msg)
+	log(msg, vim.log.levels.WARN, "WARNING")
+end
+
+---@param msg string # plain message
+M.info = function(msg)
+	log(msg, vim.log.levels.INFO, "INFO")
+end
+
+---@param msg string # debug message
+M.debug = function(msg)
+	log(msg, vim.log.levels.DEBUG, "DEBUG")
+end
+
+---@param msg string # trace message
+M.trace = function(msg)
+	log(msg, vim.log.levels.TRACE, "TRACE")
+end
+
+return M
